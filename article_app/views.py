69027befe0922a62bc289cfc9006d8d15b95b365
MohammadSiamaki <msiamaki.4@gmail.com>
2024-04-03 18:02:10 +0330

    The views for Article and Comment models are DONE

--- article_app/views.py
+++ article_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Article, Comment
+from .serializers import ArticleSerializer, CommentSerializer
 
 # Create your views here.
+
+
+class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all().order_by("headline")
+
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+    filterset_fields = ("headline", "writer", "publication", "categories")
+    search_fields = ("headline", "writer", "publication", "categories")
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+    filterset_fields = ("user_name_comment", "article_comment")
+    search_fields = ("user_name_comment", "article_comment")
